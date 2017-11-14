0364e9932265bb819b2b5d4bde70bef672287186
jay <zenghui64@qq.com>
2017-11-14 11:34:05 +0800

    提交一次

---
+++ V2EX/V2EX/Main/Tool/NetworkTool.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkTool.swift
+//  V2EX
+//
+//  Created by jay on 2017/11/2.
+//  Copyright © 2017年 曾辉. All rights reserved.
+//
+
+import UIKit
+
+protocol NetworkToolProtocol {
+}
+
+class NetworkTool: NetworkToolProtocol {
+    
+}
