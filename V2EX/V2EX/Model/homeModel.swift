59024510e08e31a15067b276982fffe730e5f830
jay <zenghui64@qq.com>
2017-12-01 18:00:04 +0800

    提交

--- V2EX/V2EX/Model/homeModel.swift
+++ V2EX/V2EX/Model/homeModel.swift
@@ -40,3 +40,72 @@ class homeModel {
 }
 
 
+/// {
+/// "id" : 254032,
+/// "username" : "86322989",
+/// "tagline" : "",
+/// "avatar_mini" : "//v2ex.assets.uxengine.net/avatar/838b/31da/254032_mini.png?m=1507543878",
+/// "avatar_normal" : "//v2ex.assets.uxengine.net/avatar/838b/31da/254032_normal.png?m=1507543878",
+/// "avatar_large" : "//v2ex.assets.uxengine.net/avatar/838b/31da/254032_large.png?m=1507543878"
+/// }
+
+import SwiftyJSON
+import BXModel
+//User(eq,hash)
+//id:i;username;tagline;avatar_mini;avatar_normal;avatar_large
+struct User :BXModel{
+    let id : Int
+    let username : String
+    let tagline : String
+    let avatarMini : String
+    let avatarNormal : String
+    let avatarLarge : String
+    
+    init(json:JSON){
+        self.id =  json["id"].intValue
+        self.username =  json["username"].stringValue
+        self.tagline =  json["tagline"].stringValue
+        self.avatarMini =  json["avatar_mini"].stringValue
+        self.avatarNormal =  json["avatar_normal"].stringValue
+        self.avatarLarge =  json["avatar_large"].stringValue
+    }
+    
+    func toDict() -> [String:Any]{
+        var dict : [String:Any] = [ : ]
+        dict["id"] = self.id
+        dict["username"] = self.username
+        dict["tagline"] = self.tagline
+        dict["avatar_mini"] = self.avatarMini
+        dict["avatar_normal"] = self.avatarNormal
+        dict["avatar_large"] = self.avatarLarge
+        return dict
+    }
+}
+
+extension User: Equatable{
+    static func ==(lhs:User,rhs:User) -> Bool{
+        return lhs.id == rhs.id
+    }
+}
+
+
+
+extension  User : Hashable{
+    var hashValue:Int{ return id.hashValue   }
+}
+
+extension User : CustomStringConvertible{
+    var description:String {
+        var builder = "User["
+        builder.append("id=\(id),")
+        builder.append("username=\(username),")
+        builder.append("tagline=\(tagline),")
+        builder.append("avatarMini=\(avatarMini),")
+        builder.append("avatarNormal=\(avatarNormal),")
+        builder.append("avatarLarge=\(avatarLarge)")
+        builder.append("]")
+        return builder
+    }
+}
+
+
